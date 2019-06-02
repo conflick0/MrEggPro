aef0cbbbe502d3746f09a63cb4301dcb44ff5004
conflick0 <jpjhsu@gms.tku.edu.tw>
2019-06-02 23:26:40 +0800

    update footer and add tab

--- JS/base.js
+++ JS/base.js
@@ -7,42 +7,20 @@ function showNav() {
     }
 }
 
-document.addEventListener("keydown", move_man, false);
+const mainHeight = document.getElementById('main').offsetHeight;/*includes padding border.*/
 
-function move_man(event) {
-    var key_event = event.key;
-    var one_punch_man = document.getElementById("one-punch-man");
-    var posY = 0, posX=0;
-    if (key_event === "ArrowUp") {
-        one_punch_man.style.transform = "rotateX(360deg)";
-        posY = one_punch_man.offsetTop - 50;
-        if(posY > 0){
-            one_punch_man.style.top = posY +"px";
-        }
-    }
-    else if (key_event === "ArrowDown") {
-        one_punch_man.style.transform = "rotateX(180deg)";
-        posY = one_punch_man.offsetTop + 50;
-        if(posY < document.body.offsetHeight-200){
-            one_punch_man.style.top = posY +"px";
-        }
+window.onscroll = function() {extend_hr()};
 
-    }
+function extend_hr() {
+    var target_pos = mainHeight-700;
+    var current_pos = document.documentElement.scrollTop;
 
-    else if (key_event === "ArrowRight") {
-        one_punch_man.style.transform = "rotate(90deg)";
-        posX = one_punch_man.offsetLeft + 50;
-        if(posX < document.body.offsetWidth-120){
-            one_punch_man.style.left = posX +"px";
-        }
-    }
-    else if (key_event === "ArrowLeft") {
-        one_punch_man.style.transform = "rotate(-90deg)";
-        posX = one_punch_man.offsetLeft - 50;
-        if(posX > 0){
-            one_punch_man.style.left = posX +"px";
-        }
-    }
 
+    if ( current_pos > target_pos ) {
+        document.getElementById("hr").className = "hr extend";
+    } else {
+        document.getElementById("hr").className = "hr";
+    }
 }
+
 
---
+++ JS/home.js
@@ -0,0 +1,8 @@
+function showNav() {
+    var x = document.getElementById("topNav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
