0aadbbdc1b9d913082e879246e483780ab8dda07
conflick0 <jpjhsu@gms.tku.edu.tw>
2019-06-09 20:56:37 +0800

    delete

--- TEST/base.js
+++
@@ -1,8 +0,0 @@
-function showNav() {
-    var x = document.getElementById("topNav");
-    if (x.className === "nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav";
-    }
-}
