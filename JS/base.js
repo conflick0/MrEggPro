35ad40a5176e7d8d4340c267840d05b5ea32da56
conflick0 <jpjhsu@gms.tku.edu.tw>
2019-06-09 20:57:49 +0800

    v.1.0

--- JS/base.js
+++ JS/base.js
@@ -7,16 +7,18 @@ function showNav() {
     }
 }
 
-const mainHeight = document.getElementById('main').offsetHeight;/*includes padding border.*/
-
 window.onscroll = function() {extend_hr()};
 
 function extend_hr() {
-    var target_pos = mainHeight-500;
-    var current_pos = document.documentElement.scrollTop;
+    var mainHeight = document.getElementById('main').offsetHeight;/*includes padding border.*/
+    var contactHeight = document.getElementById('contact-container').offsetHeight;/*includes padding border.*/
 
+    var target_pos = mainHeight-2*contactHeight;
+    var current_pos = document.documentElement.scrollTop;
+    /*document.getElementById('debug').innerHTML = target_pos +'<br>'+ current_pos + "hr" + contactHeight;*/
     if ( current_pos > target_pos ) {
         document.getElementById("hr").className = "hr extend";
+        /*document.getElementById('debug').innerHTML = target_pos +'<br>'+ current_pos + "hit" + contactHeight;*/
     } else {
         document.getElementById("hr").className = "hr";
     }
